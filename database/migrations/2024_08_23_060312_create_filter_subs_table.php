ef02a85021415416621c24ac2544c58e789d15a9
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-09-16 10:08:46 +0800

    Add new migration to update filterSubs table

--- database/migrations/2024_08_23_060312_create_filter_subs_table.php
+++ database/migrations/2024_08_23_060312_create_filter_subs_table.php
@@ -29,7 +29,6 @@ return new class extends Migration
             $table->integer('odoo_created_by_id')->nullable();
             $table->string('required_delivery')->nullable();
             $table->integer('required_delivery_updated_by_id')->nullable();
-            $table->string('category')->nullable();
         });
     }
     /**
