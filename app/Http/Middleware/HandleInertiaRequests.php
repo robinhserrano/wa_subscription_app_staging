d16995c711e774388baecc1e700fdad14e8583e0
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-09-17 12:37:55 +0800

    Fix Username issue

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -36,15 +36,7 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
-        // return array_merge(parent::share($request), [
-        //     //
-        // ]);
         return array_merge(parent::share($request), [
-            'auth' => [
-                'user' => Auth::user() ? [
-                    'username' => Auth::user()->name
-                ] : null
-            ]
         ]);
     }
 }
