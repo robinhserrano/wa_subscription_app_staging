ec4f744e278d59d12d4633cc84458c1ca87de713
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-09-20 12:06:51 +0800

    Reverted laravel name back to Laravel

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'WA Filter Sub - Ops'),
+    'name' => env('APP_NAME', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
